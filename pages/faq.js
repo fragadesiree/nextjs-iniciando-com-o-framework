70acbced84a41dc6b4546a51ab1b08a2180d6334
Desiree Fraga <fragadeh.a@gmail.com>
2025-03-05 23:12:43 -0300

    fix lint errors and change json faq file

--- pages/faq.js
+++ pages/faq.js
@@ -6,10 +6,8 @@ export default FAQScreen;
 // ISG - Incremental Static Generation
 
 export async function getStaticProps() {
-  console.log("Em modo DEV, sempre roda! A cada acesso");
-  console.log("Roda SOMENTE em build time");
   const FAQ_API_URL =
-    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+    "https://gist.githubusercontent.com/fragadesiree/573d0eb03b3ccb550a80ef102e2d127b/raw/9094aea4f62740cf6573d7c291510e7a2fb38f1a/nextjs-fake-faq.json";
   const faq = await fetch(FAQ_API_URL)
     .then((respostaDoServidor) => {
       return respostaDoServidor.json();
